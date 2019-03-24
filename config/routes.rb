b397c9ae15c44dcd07c1fe89140930eb7a87f426
kwclark <kwclark14@gmail.com>
2019-03-23 23:16:40 -0600

    Save files and fix conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,5 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  root 'static_pages/home'
-=======
   root 'static_pages#home'
->>>>>>> 5225d5a1026dc4a616eb2393ff227e1f0705c3fd
 end
